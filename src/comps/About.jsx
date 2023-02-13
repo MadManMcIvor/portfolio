d6ce88086bb42e4e4beb69f5b6c1366c675b469d
Alex McIvor <alexmcivor@Alexs-MBP.attlocal.net>
2023-02-12 21:32:39 -0800

    added the about section, but still need to finish it

--- src/comps/About.jsx
+++ src/comps/About.jsx
@@ -1,12 +1,28 @@
 
 function About() {
   return (
-    <div>
-
-
-
+    <section className="bg-gray-50 bg-opacity-20 px-40 h-auto">
       
-    </div>
+      {/* Text */}
+      <div className="text-lg text-gray-50">
+        <div className=" text-4xl pt-4 underline">
+          <h2>
+            About Me
+          </h2>
+        </div>
+        <div className="">
+          <p className="pb-2">
+          Hi there! My name’s Alex. I’m a driven full stack software engineer who loves solving complex problems with creative and practical solutions. I’m constantly on the lookout to add new skills and technologies to my repertoire. 
+          </p>
+          <p className="py-2">
+          Prior to making my way toward software engineering, I worked in the entertainment industry in LA giving it my best shot as a comedy screenwriter. Along the way, I also made a stop in the real estate industry as a Realtor. 
+          </p>
+          <p className="py-2">
+          When I’m not banging my head against the wall working towards solving the latest bug du jour in my code, I’m either reading, playing board games, solving puzzles, cooking, or just hanging with my wife and our doggo, Bailey. 
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 

d0023ad126e627537b9f2dffb44d196c421558ea
Alex McIvor <alexmcivor@Alexs-MBP.attlocal.net>
2023-02-14 12:24:26 -0800

    reworked the icons so they're interactice

--- src/comps/SkillElement.jsx
+++ src/comps/SkillElement.jsx
@@ -2,9 +2,13 @@
 function SkillElement({icon, text}) {
   return (
     <>
-        <div className='text-7xl text-white bg-gradient-to-r from-emerald-600 to-sky-600 inline-block px-2 py-1 m-2 text-center'>
-        {icon}
-        <h3 className='text-lg'>{text}</h3>
+        <div className='bg-gradient-to-r from-emerald-600 to-sky-600 inline-block px-2 py-1 m-2 text-center'>
+          <div className="max-w-[100px] mx-auto transform transition duration-500 hover:scale-110 py-1">
+            {icon}
+          </div>
+        <div className="flex justify-center">
+          <h3 className=' text-white text-lg text-center'>{text}</h3>
+        </div>
       </div>
     </>
     )
--- src/comps/Skills.jsx
+++ src/comps/Skills.jsx
@@ -7,28 +7,33 @@ import SkillElement from './SkillElement'
 
 function Skills() {  
   return (
-  <section className='bg-gray-50 md:px-40 px-10 h-auto py-10'>
-    <div className="text-5xl pt-4 text-center">
+  <section className='bg-gray-50 md:px-28 px-10 h-auto py-10'>
+
+    <div className="text-5xl text-center pb-2">
       <h2 className='bg-gradient-to-r from-emerald-600 to-sky-600 inline-block text-transparent bg-clip-text'>
         Skills
       </h2>
     </div>
-    <div className='flex-wrap justify-center'>
-      <SkillElement icon={<FaReact/>} text="React" />
-      <SkillElement icon={<FaHtml5/>} text="HTML" />
-      <SkillElement icon={<FaCss3Alt/>} text="CSS" />
-      <SkillElement icon={<FaJsSquare/>} text="ES6" />
-      <SkillElement icon={<FaDocker/>} text="Docker" />
-      <SkillElement icon={<FaPython/>} text="Python" />
-      <SkillElement icon={<FaGit/>} text="git" />
-      <SkillElement icon={<SiTailwindcss/>} text="Tailwind" />
-      <SkillElement icon={<SiDjango/>} text="Django" />
-      <SkillElement icon={<SiExpress/>} text="Express" />
-      <SkillElement icon={<SiFastapi/>} text="FastAPI" />
-      <SkillElement icon={<SiMysql/>} text="MySQL" />
-      <SkillElement icon={<SiPostgresql/>} text="Postgres" />
-      <SkillElement icon={<SiMongodb/>} text="Mongo" />
+    
+    <div className='flex flex-row justify-center'>
+      <div className="flex flex-wrap w-full justify-center rounded bg-gray-200 bg-opacity-80">
+        <SkillElement icon={<FaReact size={80} style={{ color: "white" }} />} text="React" />
+        <SkillElement icon={<FaHtml5 size={80} style={{ color: "white" }} />} text="HTML" />
+        <SkillElement icon={<FaCss3Alt size={80} style={{ color: "white" }} />} text="CSS" />
+        <SkillElement icon={<FaJsSquare size={80} style={{ color: "white" }} />} text="ES6" />
+        <SkillElement icon={<FaDocker size={80} style={{ color: "white" }} />} text="Docker" />
+        <SkillElement icon={<FaPython size={80} style={{ color: "white" }} />} text="Python" />
+        <SkillElement icon={<FaGit size={80} style={{ color: "white" }} />} text="git" />
+        <SkillElement icon={<SiTailwindcss size={80} style={{ color: "white" }} />} text="Tailwind" />
+        <SkillElement icon={<SiDjango size={80} style={{ color: "white" }} />} text="Django" />
+        <SkillElement icon={<SiExpress size={80} style={{ color: "white" }} />} text="Express" />
+        <SkillElement icon={<SiFastapi size={80} style={{ color: "white" }} />} text="FastAPI" />
+        <SkillElement icon={<SiMysql size={80} style={{ color: "white" }} />} text="MySQL" />
+        <SkillElement icon={<SiPostgresql size={80} style={{ color: "white" }} />} text="Postgres" />
+        <SkillElement icon={<SiMongodb size={80} style={{ color: "white" }} />} text="Mongo" />
+      </div>
     </div>
+
   </section>
   )
 }
